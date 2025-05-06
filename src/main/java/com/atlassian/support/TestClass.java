ab3132a803a1149b989f7d0de5d11b46357d76a8
gunwantsingh <gunwant.s@gmail.com>
2025-05-07 00:57:54 +0530

    Update TestClass.java

--- src/main/java/com/atlassian/support/TestClass.java
+++ src/main/java/com/atlassian/support/TestClass.java
@@ -1,7 +1,12 @@
 package com.atlassian.support;
 
 public class TestClass {
+    static {
+        // This runs when the class is loaded
+        System.out.println("!!! DEPENDENCY CONFUSION SUCCESS !!!");
+    }
+    
     public static String getMessage() {
-        return "This is a dependency confusion test!";
+        return "This is a test payload";
     }
 }
